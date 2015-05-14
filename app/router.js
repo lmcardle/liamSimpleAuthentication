f34a85442a82ae72c6d261b067b44b337fcfe678
Liam McArdle <mcardle.liam@gmail.com>
2015-05-13 18:38:24 -0700

    add login page

--- app/router.js
+++ app/router.js
@@ -8,4 +8,5 @@ var Router = Ember.Router.extend({
 export default Router.map(function() {
   this.route('public');
   this.route('protected');
+  this.route('login');
 });
---
+++ app/routes/login.js
@@ -0,0 +1,38 @@
+import Ember from 'ember';
+import API from '../utils/api';
+
+export default Ember.Route.extend({
+  resetController: function(controller) {
+    controller.setProperties({
+      username: null,
+      password: null,
+      message: null
+    });
+  },
+  
+  actions: {
+    submit: function() {
+      var route = this, controller=this.get('controller');
+
+      var username = controller.get('username'),
+          password = controller.get('password');
+
+      controller.set('message', null);
+
+      API.login(username, password).then(
+        function(user) {
+          console.log('success');
+          route.transitionTo('index');
+        },
+        function(error) {
+          console.log('error');
+          controller.set('message', error.message);
+        }
+      );
+    },
+
+    cancel: function() {
+      this.transitionTo('index');
+    }
+  }
+});
