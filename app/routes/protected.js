055044c212bd64a219e2de5983d8cc7f9076ebb8
Liam McArdle <mcardle.liam@gmail.com>
2015-05-13 23:30:16 -0700

    if the user is not logged in, do not even try to make the get request to get the protected page content

--- app/routes/protected.js
+++ app/routes/protected.js
@@ -2,6 +2,15 @@ import Ember from 'ember';
 import API from '../utils/api';
 
 export default Ember.Route.extend({
+  beforeModel: function() {
+    if (!this.session.get('user')) {
+      return Ember.RSVP.reject({
+        status: 'Unauthorized',
+        message: 'Please login to access this page'
+      });
+    }
+  },
+
   model: function() {
     return API.get('api/protected');
   }
